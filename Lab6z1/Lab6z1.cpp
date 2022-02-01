cffec68dc8b0352ddb1f8066433d972757e19b34
SmolPrzybs <maciek970@gmail.com>
2022-02-01 23:48:18 +0100

    Dodaj pliki projektów.

---
+++ Lab6z1/Lab6z1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream> 
+using namespace std;
+int main() {
+
+	int* ptr, * ptr1, * ptr2, a = 1342, b = 233, c = 2;
+	ptr = &a; 
+	ptr1 = &b;
+	ptr2 = &c;
+	cout << "roznica: " << (*ptr) - (*ptr1) << endl;
+	cout << "srednia: " << (double)((*ptr) + (*ptr1) + (*ptr2)) / 3 << endl; 
+	cout << "adres: " << ptr << endl; 
+	cout << "wartosc: " << *ptr << endl;
+}
